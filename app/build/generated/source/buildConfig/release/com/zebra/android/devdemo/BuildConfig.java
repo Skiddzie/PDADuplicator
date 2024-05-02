08b7793336934ed1744e3f424cdc5f94a0ac3d33
Skiddzie <aidanthe1@gmail.com>
2024-05-02 18:25:15 -0400

    package name

--- app/build/generated/source/buildConfig/release/com/zebra/android/devdemo/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/zebra/android/devdemo/BuildConfig.java
@@ -5,8 +5,8 @@ package com.zebra.android.devdemo;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.zebra.android.duplicator";
+  public static final String APPLICATION_ID = "com.zebra.android.barcode.duplicator";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 2;
+  public static final int VERSION_CODE = 3;
   public static final String VERSION_NAME = "1.1";
 }
